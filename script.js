f2d8088e87dc69892833ad2c5bed3fa44437b613
TCodes <theodoreuokeke@gmail.com>
2023-06-29 20:10:40 +0100

    added JS code

---
+++ script.js
@@ -0,0 +1,37 @@
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+
+var rockPaperScissor = ['rock', 'paper', 'scissor'];
+var user = window.prompt('select rock, paper or scissor');
+var computer =math.floor(math.random(rockPaperScissor.length));
+
+if (user === computer) {
+    window.alert('it is a tie!')
+    ties+= 1
+}
+else if (user === 'rock' && computer === 'paper'){
+    window.alert('You lose')
+    losses+= 1
+}
+else if (user === 'paper' && computer === 'rock') {
+    window.alert('You win')
+    wins+= 1
+}
+else if (user === 'rock' && computer === 'scissor') {
+    window.alert('You win')
+    wins+= 1
+}
+else if (user === 'scissor' && computer === 'rock') {
+    window.alert('You lose')
+    losses+= 1
+}
+else if (user === 'scissor' && computer === 'paper') {
+    window.alert('You win')
+    wins+= 1
+}
+else if (user === 'paper' && computer === 'scissor') {
+    window.alert('You lose')
+    losses+= 1
+}
